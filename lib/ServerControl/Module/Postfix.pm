1af5ee259c4a613bd51d3d16e2c33581d7a5cb16
Daniel Bäurer <daniel.baeurer@web.de>
2012-01-04 17:40:19 +0100

    - Changes: Increase Version

--- lib/ServerControl/Module/Postfix.pm
+++ lib/ServerControl/Module/Postfix.pm
@@ -9,7 +9,7 @@ package ServerControl::Module::Postfix;
 use strict;
 use warnings;
 
-our $VERSION = '0.94';
+our $VERSION = '0.95';
 
 use ServerControl::Module;
 use ServerControl::Commons::Process;
